fa52cccf9cf8fc90b7eae3a0a618b0b7f0f0a55d
Dylan Djian <ddjian@student.42.fr>
2019-05-28 23:51:21 +0200

    modify socket front for heroku

--- src/client/socket.js
+++ src/client/socket.js
@@ -1,5 +1,4 @@
 import io from "socket.io-client"
-const socket = io("http://localhost:8081/")
-//  const socket = io("http://167.99.33.85:8081/")
+const socket = io("http://localhost:8080/")
 
 export { socket }
